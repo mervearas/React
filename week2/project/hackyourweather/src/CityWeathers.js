1abdc845fbb09f6f3c3d3410613d62f13ffda736
mervearas <mervegurzel@icloud.com>
2020-11-18 01:27:29 +0100

    removed the key

--- week2/project/hackyourweather/src/CityWeathers.js
+++ week2/project/hackyourweather/src/CityWeathers.js
@@ -10,7 +10,7 @@ const CityWeathers = () => {
     function getCityWeather(city) {
         setLoading(true);
         setError(false);
-        fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.REACT_APP_OPENWEATHERMAP_API_KEY}`)
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.REACT_APP_OPENWEATHERMAP_API_KEY}`)
             .then(res => res.json())
             .then(res => {
                 if(res.cod === "404") {
